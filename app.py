68c143d24f83871cdd6949e45136cc7ea96b72f8
Henrico Bela <henrico.developer@gmail.com>
2023-07-10 23:24:20 -0300

    Switch page

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def switch_page(page_name: str):
 
     page_name = standardize_name(page_name)
 
-    pages = get_pages("streamlit_app.py")  # OR whatever your main page is called
+    pages = get_pages("app.py")
 
     for page_hash, config in pages.items():
         if standardize_name(config["page_name"]) == page_name:
