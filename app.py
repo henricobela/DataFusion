ef63cf2a9df5975782ab47a1398d152276985b38
Henrico Bela <henrico.developer@gmail.com>
2023-07-15 19:55:06 -0300

    Teste

--- app.py
+++ app.py
@@ -9,9 +9,8 @@ def switch_page(page_name: str):
 
     def standardize_name(name: str) -> str:
         return name.lower().replace("_", " ")
-
+    
     page_name = standardize_name(page_name)
-
     pages = get_pages("app.py")
 
     for page_hash, config in pages.items():
@@ -22,7 +21,7 @@ def switch_page(page_name: str):
                     page_name=page_name,
                 )
             )
-
+        
     page_names = [standardize_name(config["page_name"]) for config in pages.values()]
 
     raise ValueError(f"Could not find page {page_name}. Must be one of {page_names}")
